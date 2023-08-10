f2f970b4ce1e41c84c7cc75e2fb4e7b57a4296ec
Laxmikant Saraswat <laxmikantsarswat007@gmail.com>
2023-08-10 19:07:31 +0530

    Vercel Backend Added script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
 
   loader(messageDiv);
-  const response = await fetch('https://codex-backend-3wci.onrender.com/', {
+  const response = await fetch('https://codex-server-seven.vercel.app/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
